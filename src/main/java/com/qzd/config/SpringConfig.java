51d042ef798ef9d34ce4b48be5cabbbdcd7c91e5
Zongdi <690160142@qq.com>
2023-03-01 17:50:18 +0800

    按注解排除需要加载的类 方式spring 加载spring mvc控制的类

--- src/main/java/com/qzd/config/SpringConfig.java
+++ src/main/java/com/qzd/config/SpringConfig.java
@@ -2,9 +2,19 @@ package com.qzd.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Controller;
 
 @Configuration
-@ComponentScan({"com.qzd.dao","com.qzd.service"})
+//@ComponentScan({"com.qzd.dao","com.qzd.service"})
+@ComponentScan(value = "com.qzd",
+    excludeFilters = @ComponentScan.Filter(
+            type = FilterType.ANNOTATION,
+            classes = Controller.class
+    )
+
+
+)
 public class SpringConfig {
 
 }
