17fec1b18d24ced33c703cfc993668f2bcd2748f
Zongdi <690160142@qq.com>
2023-03-01 17:01:35 +0800

    设置Controller和 Config Config 扫描controller类的方法

---
+++ src/main/java/com/qzd/config/SpringMvcConfig.java
@@ -0,0 +1,10 @@
+package com.qzd.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.qzd.controller")
+
+public class SpringMvcConfig {
+}
