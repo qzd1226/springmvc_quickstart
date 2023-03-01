9facedbf995287b3105da37b4120215e3d9cf85f
Zongdi <690160142@qq.com>
2023-03-01 17:20:38 +0800

    测试成功 将save方法设置为响应体 @ResponseBody 我们可以在网页中得到json数据字符串

--- src/main/java/com/qzd/controller/UserController.java
+++ src/main/java/com/qzd/controller/UserController.java
@@ -2,13 +2,16 @@ package com.qzd.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserController {
 
     @RequestMapping("/save")
-    public void save(){
+    @ResponseBody
+    public String save(){
         System.out.println("user save....");
+        return "{'info':'springmvc'}";
 
     }
 }
